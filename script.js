fc123335934db1044bdb69d796123808a52ac600
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2024-06-11 13:09:30 +0200

    update arrays on local storage to reflect drag and drop changes

--- script.js
+++ script.js
@@ -49,10 +49,6 @@ function updateSavedColumns() {
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-//   console.log('columnEl:', columnEl);
-//   console.log('column:', column);
-//   console.log('item:', item);
-//   console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
@@ -61,7 +57,6 @@ function createItemEl(columnEl, column, item, index) {
   listEl.setAttribute('ondragstart', 'drag(event)');
   // Append
   columnEl.appendChild(listEl);
-
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
@@ -95,14 +90,34 @@ if (!updatedOnLoad) {
   });
 
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
 
-
+// Allow array to reflect Drag and Drop items
+function rebuildArrays() {
+    backlogListArray = [];
+    for (let i = 0; i < backlogList.children.length; i++) {
+      backlogListArray.push(backlogList.children[i].textContent);
+    }
+    progressListArray = [];
+    for (let i = 0; i < progressList.children.length; i++) {
+      progressListArray.push(progressList.children[i].textContent);
+    }
+    completeListArray = [];
+    for (let i = 0; i < completeList.children.length; i++) {
+      completeListArray.push(completeList.children[i].textContent);
+    }
+    onHoldListArray = [];
+    for (let i = 0; i < onHoldList.children.length; i++) {
+      onHoldListArray.push(onHoldList.children[i].textContent);
+    }
+    updateDOM();
 }
 
 // When Item Starts Dragging
 function drag(e) {
     draggedItem = e.target;
-    console.log('draggedItem:', draggedItem);
 }
 
 // Column Allows for Item to Drop
@@ -126,6 +141,7 @@ function drop(e) {
     // Add item to Column
     const parent = listColumns[currentColumn];
     parent.appendChild(draggedItem);
+    rebuildArrays();
 }
 
 // On Load
